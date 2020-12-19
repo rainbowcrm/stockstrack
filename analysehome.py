8e7b230bc86f54d3fd47e1cd146699d5eb9fb6d5
Unnikrishnan Nirmala <unnikrishnan.dn@gmail.com>
2020-12-19 16:50:06 +0530

    minor changes

--- analysehome.py
+++ analysehome.py
@@ -6,6 +6,7 @@ from stockclass import StockTransaction
 import urllib.request, json
 from datetime import datetime
 import pandas as pd
+from sqlalchemy.sql import func
 
 engine = create_engine('mysql://stockuser:delhi@localhost:3306/stocks')
 Session = sessionmaker(bind=engine)
@@ -17,7 +18,7 @@ end_day = input("Please enter end date  in YYYY-mm-dd format")
 start_trade_day = datetime.strptime(from_day,"%Y-%m-%d")
 end_trade_day = datetime.strptime(end_day,"%Y-%m-%d")
 all_records =[] 
-for stock in session.query(Stock).filter( Stock.is_tracked == True).order_by(Stock.id):
+for stock in session.query(Stock).filter(Stock.is_tracked == True ).order_by(Stock.id):
     start_price = 1
     end_price = 1
     for trans in  session.query(StockTransaction).filter(and_(StockTransaction.stock_id == stock.id, \
@@ -28,16 +29,28 @@ for stock in session.query(Stock).filter( Stock.is_tracked == True).order_by(Sto
         StockTransaction.trans_date == end_trade_day )).order_by(StockTransaction.trans_date):
         end_price = transClose.close_price
 
+    aggrValue =  session.query(func.max(StockTransaction.close_price),func.min(StockTransaction.close_price),func.avg(StockTransaction.close_price)).filter(and_(StockTransaction.stock_id == stock.id, \
+        StockTransaction.trans_date <= end_trade_day, StockTransaction.trans_date >= start_trade_day)).all()
+
+     
+    min_price = aggrValue[0][1]
+    max_price = aggrValue[0][0]
+    avg_price = aggrValue[0][2]
+    
+    percent_variation =1
     percent_increase = ((end_price - start_price) / start_price) * 100
+    if ( not (min_price  is None) and  not (max_price  is None)):
+        percent_variation = ((max_price - min_price) / min_price) * 100
     #print("Stock = " +  stock.security_name  + ":start=" + str(start_price) + ": end=" + str(end_price) + ":incr=" + str(percent_increase))
-    ind_record =[stock.security_name,stock.industry,start_price,end_price,percent_increase]
+    ind_record =[stock.security_name,stock.industry,start_price,end_price,percent_increase,min_price,max_price,avg_price,percent_variation]
     if (start_price != 1 and end_price != 1):
         all_records.append(ind_record)
 
-df = pd.DataFrame(all_records,columns=['Stock','Industry','Start','End','Percent'])
+df = pd.DataFrame(all_records,columns=['Stock','Industry','Start','End','Percent','Min','Max','Avg','Var'])
 sorted_rows = df.sort_values(by=['Percent'])
-for currow in sorted_rows.iterrows():
-    print (currow)
+sorted_rows.to_csv('f2.csv',index=False)
+#for currow in sorted_rows.iterrows():
+    #print (currow)
     #print(currow['Stock'] + ' ' + currow['Industry'] + ' ' + currow['Start'] + ' ' + currow['End'] + ' ' +  currow['Percent']  )
-#print(df) 
+#print(df)
 
--- stocktranspresent.py
+++ stocktranspresent.py
@@ -22,8 +22,16 @@ def get_stock_transaction(api_code, security_name, stock_id , daily_data,date_ke
 
 
 def get_time_series(response_data):
-    daily_data = response_data[time_series_key]
-    return daily_data
+    if time_series_key in response_data:
+        daily_data = response_data[time_series_key]
+        return daily_data
+    else:
+        return ''
+
+def update_tracked(session,cur_stock):
+    cur_stock.is_tracked = True
+    session.commit()
+    print("updating is trakced to true " + cur_stock.security_id )
 
 
 engine = create_engine('mysql://stockuser:delhi@localhost:3306/stocks')
@@ -35,23 +43,29 @@ url_part2 = "&outputsize=120&apikey=3N39E9EDEQGEWWBM"
 #MC90XH32PC655W2S 3N39E9EDEQGEWWBM
 # select * from  stock_master where groupc ='A ' or groupc = 'B ' and id < 700 -- upto Nov 27 
 
-for stock in session.query(Stock).filter(and_(Stock.groupc == 'B ' ,Stock.id < 700,Stock.id >= 398)).order_by(Stock.id):
+for stock in session.query(Stock).filter(and_(Stock.groupc == 'X ' ,Stock.id < 2000,Stock.id >= 1812 )).order_by(Stock.id):
     #stock.print_content()
     url = url_part1 + stock.api_code + url_part2
     print(stock.security_name) 
+    print(stock.id) 
+    
     time.sleep(2)
     response = urllib.request.urlopen(url)
     data = json.loads(response.read())
 
     daily_data = get_time_series(data)
-    keys_list = ["2020-12-01","2020-12-02","2020-12-03","2020-12-04"]
-    for index in range(0,4):
+    if (daily_data == ''):
+        continue
+    keys_list = ["2020-12-07","2020-12-08","2020-12-09","2020-12-10","2020-12-11","2020-12-14","2020-12-15","2020-12-16","2020-12-17","2020-12-18"]
+    for index in range(0,len(keys_list)):
         date_key = keys_list[index]
         print(date_key)
-        print(daily_data[date_key])
-        stock_transaction = get_stock_transaction(stock.api_code,stock.security_name,stock.id,daily_data[date_key],date_key)
-        session.add(stock_transaction)
-        session.commit()
+        if(date_key in daily_data):
+            print(daily_data[date_key])
+            stock_transaction = get_stock_transaction(stock.api_code,stock.security_name,stock.id,daily_data[date_key],date_key)
+            session.add(stock_transaction)
+            session.commit()
+            update_tracked(session,stock)
         #stock_transaction.print_content()
         
     
